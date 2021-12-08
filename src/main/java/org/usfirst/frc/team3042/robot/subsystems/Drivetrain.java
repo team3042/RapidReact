e97df49adf55b9c5b2a89f76d711f4ab08b66eec
poobanaaselvam <poobanaaselvam@gmail.com>
2021-12-07 20:59:35 -0600

    Merged DrivetrainEncoders into Drivetrain (12.7.2021)
    - Moved over the imports from DrivetrainEncoder
    - Added new Configuration Constants
    - Added new Instance Variables
    - Moved all the Encoder Methods over from DrivetrainEncoder
    - Moved the lines of code under the Method "initDefaultCommand" from DrivetrainEncoder
    - Changed the old DrivetrainEncoder main method into a regular method by adding void into the initializing line of the method

--- src/main/java/org/usfirst/frc/team3042/robot/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team3042/robot/subsystems/Drivetrain.java
@@ -8,6 +8,10 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -30,6 +34,13 @@ public class Drivetrain extends Subsystem {
 	private static final boolean REVERSE_RIGHT_MOTOR = RobotMap.REVERSE_RIGHT_MOTOR;	
 	private static final int SOLENOID_ID = RobotMap.DRIVETRAIN_SOLENOID;
 	
+	private static final int COUNTS_PER_REVOLUTION = RobotMap.COUNTS_PER_REVOLUTION;
+	private static final int FRAME_RATE = RobotMap.ENCODER_FRAME_RATE;
+	private static final int TIMEOUT = RobotMap.AUTON_TIMEOUT;
+	private static final int PIDIDX = RobotMap.AUTON_PIDIDX;
+	private static final boolean SENSOR_PHASE_LEFT = RobotMap.SENSOR_PHASE_LEFT;
+	private static final boolean SENSOR_PHASE_RIGHT = RobotMap.SENSOR_PHASE_RIGHT;
+
 	/** Instance Variables ****************************************************/
 	Log log = new Log(LOG_LEVEL, SendableRegistry.getName(this));
 
@@ -46,6 +57,9 @@ public class Drivetrain extends Subsystem {
 	DifferentialDriveOdometry odometry; // Odometry class for tracking robot posistion
 
 	Boolean isHighGear = false;
+
+	TalonSRX leftEncoder, rightEncoder;
+	double leftPositionZero, rightPositionZero;
 	
 	/** Drivetrain ************************************************************
 	 * Set up the talons for desired behavior. */
@@ -75,6 +89,7 @@ public class Drivetrain extends Subsystem {
 	 * Set the default command for the subsystem. */
 	public void initDefaultCommand() {
 		setDefaultCommand(new Drivetrain_TankDrive());
+		setDefaultCommand(null);
 	}
 	
 	/** Methods for setting the motors in % power mode ********************/
@@ -145,5 +160,81 @@ public class Drivetrain extends Subsystem {
 	/** Give commands access to the drivetrain encoders ****************/
 	public DrivetrainEncoders getEncoders() {
 		return encoders;
+	}
+
+	/** DrivetrainEncoders ****************************************************/
+	public void DrivetrainEncoders(TalonSRX leftMotor, TalonSRX rightMotor) {
+		log.add("Constructor", LOG_LEVEL);
+		
+		leftEncoder = leftMotor;
+		rightEncoder = rightMotor;
+				
+		initEncoder(leftEncoder, SENSOR_PHASE_LEFT);
+		initEncoder(rightEncoder, SENSOR_PHASE_RIGHT);
+													
+		reset();
+	}
+	private void initEncoder(TalonSRX encoder, boolean sensorPhase) {
+		encoder.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, PIDIDX, TIMEOUT);
+		encoder.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, FRAME_RATE, TIMEOUT);
+		encoder.setSensorPhase(sensorPhase); // affects closed-loop mode
+	}
+
+	/** reset *****************************************************************/
+	public void reset() {
+		int leftCounts = (int)(leftEncoder.getSelectedSensorPosition(PIDIDX));
+		leftPositionZero = countsToRev(leftCounts);
+		
+		int rightCounts = (int)(rightEncoder.getSelectedSensorPosition(PIDIDX));
+		rightPositionZero = countsToRev(rightCounts);
+	}
+	public void setToZero() {
+		leftPositionZero = 0.0;
+		rightPositionZero = 0.0;
+	}
+	
+	/** Get the encoder position or speed *************************************
+	 * Position is converted to revolutions
+	 * Speed returns counts per 100ms and is converted to RPM */
+	public double getLeftPosition() {
+		int counts = (int)(leftEncoder.getSelectedSensorPosition(PIDIDX));
+		return countsToRev(counts) - leftPositionZero;
+	}
+	public double getRightPosition() {
+		int counts = (int)(rightEncoder.getSelectedSensorPosition(PIDIDX));
+		return countsToRev(counts) - rightPositionZero;
+	}
+	private double countsToRev(int counts) {
+		return (double)counts / COUNTS_PER_REVOLUTION;
+	}
+	public double getLeftSpeed() {
+		int cp100ms = (int)(leftEncoder.getSelectedSensorVelocity(PIDIDX));
+		return cp100msToRPM(cp100ms);
+	}
+	public double getRightSpeed() {
+		int cp100ms = (int)(rightEncoder.getSelectedSensorVelocity(PIDIDX));
+		return cp100msToRPM(cp100ms);
+	}
+	private double cp100msToRPM(int cp100ms) {
+		return (double)cp100ms * 10.0 * 60.0 / COUNTS_PER_REVOLUTION;
+	}
+	
+	/** rpmToF ****************************************************************
+	 * Convert RPM reading into an F-Gain
+	 * Note that 1023 is the native full-forward power of the talons, 
+	 * equivalent to setting the power to 1.0.
+	 * The speed has to be converted from rpm to encoder counts per 100ms
+	 * so F = power * 1023 / speed */
+	public double rpmToF(double rpm, double power) {
+		//Convert to counts per 100 ms
+		double speed = rpm * 4.0 * COUNTS_PER_REVOLUTION / 600.0;
+		double kF = power * 1023.0 / speed;
+		return kF;
+	}
+	public double rpmToPower(double rpm, double kF) {
+		//Convert to counts per 100 ms
+		double speed = rpm * 4.0 * COUNTS_PER_REVOLUTION / 600.0;
+		double power = kF * speed / 1023.0;
+		return power;
 	}
 }
